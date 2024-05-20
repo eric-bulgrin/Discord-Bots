2c1c324eda4cfba40e43013898dbd0a9fc25f4cc
Eric Bulgrin <60482540+eric-bulgrin@users.noreply.github.com>
2024-05-20 19:12:28 -0400

    Update apikeys.py

--- Discord Bots/SandySunsetAlert/apikeys.py
+++ Discord Bots/SandySunsetAlert/apikeys.py
@@ -1,4 +1,4 @@
-token = 'MTI0MTQxOTc1ODc0MjY2NzM3OA.GDMs7J.gORzU2XcRR6FPBTBZqQJuNEw78LP_OJi2biphQ'
-guildId = '1229577233199399053'
-clientId = '1241419758742667378'
-channelId = '1241746280338948188'
+token = ''
+guildId = ''
+clientId = ''
+channelId = ''
