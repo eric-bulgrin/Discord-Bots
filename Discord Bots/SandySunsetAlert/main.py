37115b1db37c432956c4020d33c8556928620cd6
Eric Bulgrin <60482540+eric-bulgrin@users.noreply.github.com>
2024-05-21 20:23:22 -0400

    Update main.py

--- Discord Bots/SandySunsetAlert/main.py
+++ Discord Bots/SandySunsetAlert/main.py
@@ -2,24 +2,33 @@ import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import requests
+from requests.auth import HTTPDigestAuth
+import json
 
 from apikeys import *
 
+import logging
+
+logging.basicConfig(filename='logs.log',
+                    filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 class server:
-  def __init__(self, servername, map, gamemode, playercount, newserver):
+  def __init__(self, servername, servermap, gamemode, playercount, maxplayers, newserver):
     self.servername = servername
-    self.map = map
+    self.servermap = servermap
     self.gamemode = gamemode
     self.playercount = playercount
+    self.maxplayers = maxplayers
     self.newserver = newserver
 
   def __key(self):
     return self.servername
 
   def __repr__(self):
-    return "\nServer: % s \nMap: % s \nGamemode: % s \nPlayercount: % s \nNewServer: % s" % (self.servername, self.map, self.gamemode, self.playercount, self.newserver)
+    return "\nServer: % s \nMap: % s \nGamemode: % s \nPlayercount: % s \nMaxPlayers: % s" % (self.servername, self.servermap, self.gamemode, self.playercount, self.maxplayers)
   
   def __hash__(self):
     return hash(self.__key)
@@ -27,35 +36,36 @@ class server:
   def __eq__(self, other):
     return self.servername == other.servername
 
-def getRefresh (oldList, driver):
-  
+def getApiResponse(oldList):
   newList = []
-  
-  driver.refresh()
-  driver.implicitly_wait(2)
-  servernames = driver.find_elements(By.XPATH, "//td[2]")
-  maps = driver.find_elements(By.XPATH, "//td[3]")
-  gamemodes = driver.find_elements(By.XPATH, "//td[5]")
-  playercounts = driver.find_elements(By.XPATH, "//td[6]")
 
-  for i in range(len(maps)):
-    newList.append(server(servernames[i].text, maps[i].text, gamemodes[i].text, playercounts[i].text, True))
+  if len(oldList) > 0:
+    for i in range(len(oldList)):
+      oldList[i].newserver = False
+
+  url = "https://publicapi.battlebit.cloud/Servers/GetServerList"
+  myResponse = requests.get(url)
+
+  if (myResponse.ok):
+    jData = json.loads(myResponse.content)
+    
+    for item in jData:
+      newList.append(server(item["Name"], item["Map"], item["Gamemode"], item["Players"], item["MaxPlayers"], True))
+  else:
+    logging.info('API status code was %s, not 200', myResponse.status_code)
+    return oldList
 
   for i in reversed(range(len(newList))):
-    if newList[i].map != "SandySunset":
+    if newList[i].servermap != "SandySunset":
       newList.pop(i)
     elif newList[i].gamemode != "CONQ" and newList[i].gamemode != "INFCONQ" and newList[i].gamemode != "FRONTLINE":
       newList.pop(i)
-    elif int(newList[i].playercount.split('/')[0]) <= 128:
+    elif newList[i].playercount <= 128:
       newList.pop(i)
 
   finishedServers = [item for item in oldList if item not in newList]
   for item in finishedServers:
     oldList.remove(item)
-
-  if len(oldList) > 0:
-    for i in range(len(oldList)):
-      oldList[i].newserver = False
 
   for item1 in oldList:
     for item2 in newList:
@@ -72,25 +82,24 @@ class MyClient(discord.Client):
     super().__init__(*args, **kwargs)
 
     self.masterList = []
-    self.driver = webdriver.Edge()
-    self.driver.get("https://bbspy.net/")
 
   async def setup_hook(self) -> None:
     self.my_background_task.start()
 
   async def on_ready(self):
-    print(f'Logged in as {self.user} (ID: {self.user.id})')
-    print('--------------------------------------------')
+    logging.info(f'Logged in as {self.user} (ID: {self.user.id})')
+    logging.info('--------------------------------------------')
 
-  @tasks.loop(seconds=10)
+  @tasks.loop(seconds=5)
   async def my_background_task(self):
     channel = self.get_channel(int(channelId))
-
-    self.masterList = getRefresh(self.masterList, self.driver)
+    
+    self.masterList = getApiResponse(self.masterList)
 
     if len(self.masterList) > 0:
       for i in range(len(self.masterList)):
         if self.masterList[i].newserver == True:
+          logging.info(f'A New SandySunset Server Just Started:{self.masterList[i]}')
           await channel.send(f'A New SandySunset Server Just Started:{self.masterList[i]}')
 
   @my_background_task.before_loop
